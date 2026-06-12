545facf297376ae6e33b84826adb3655c340a94b
Muhammad Andi Kusuma <kusumamuhammadandi@gmail.com>
2026-06-12 18:49:23 +0700

    fix gltf base URL

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader.jsx";
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("/desktop_pc/scene.gltf");
+  const computer = useGLTF(`${import.meta.env.BASE_URL}desktop_pc/scene.gltf`);
   return (
     <mesh>
       <hemisphereLight intensity={2.5} groundColor="black" />

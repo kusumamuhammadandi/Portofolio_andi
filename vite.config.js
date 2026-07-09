8cddb20c55a8834a6fc931107ed35c54df66131f
Muhammad Andi Kusuma <kusumamuhammadandi@gmail.com>
2026-07-09 14:05:20 +0700

    enable sourcemap

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === "build" ? "/Portofolio_andi/" : "/",
+  build: {
+    sourcemap: true,
+  },
 }));
